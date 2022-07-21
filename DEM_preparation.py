ab9c658e7f6ec5ae6d04c9f47ed82c704872ad79
Clemente Gotelli <96237432+cgotelli@users.noreply.github.com>
2022-07-21 15:18:06 +0200

    Fully working
    first fully working version.

--- DEM_preparation.py
+++ DEM_preparation.py
@@ -9,9 +9,9 @@ Created on Wed Jul 20 09:00:18 2022
 
 import functions as f
 
+ortoPath = "/mnt/data2/IBER/Ortophotos/"
+rasterPath = "/mnt/data2/IBER/DEMs/"
 
-src  = open('/mnt/data2/20220719/halle_water_20220719-01_20220719T1546_dsm.tif')
-orto = open('/mnt/data2/20220719/orto_grande.tif')
-data = f.resample_raster(src)
-ortoscaled = f.resample_orto(orto)
+f.scaleOrto(ortoPath)
+f.scaleRaster(rasterPath)
 
--- functions.py
+++ functions.py
@@ -10,73 +10,100 @@ from rasterio.plot import show
 from rasterio import Affine, open
 from rasterio.enums import Resampling
 import numpy as np
+from os.path import isfile, join, exists
+from os import listdir
 
 
-def resample_raster(raster, scaleHeight=40, scaleResolution = 1/2):
+def resample_raster(raster, name, scaleHeight=40, scaleResolution=1 / 2):
     t = raster.transform
 
     # rescale the metadata
-    transform = Affine(t.a * scaleHeight/scaleResolution, t.b, t.c, t.d, t.e * scaleHeight/scaleResolution, t.f)
+    transform = Affine(
+        t.a * scaleHeight / scaleResolution, t.b, t.c, t.d, t.e * scaleHeight / scaleResolution, t.f
+    )
     height = int(raster.height * scaleResolution)
     width = int(raster.width * scaleResolution)
 
     profile = raster.profile
-    profile.update(transform=transform, driver='GTiff', height=height, width=width)
+    profile.update(transform=transform, driver="GTiff", height=height, width=width)
 
-    data = raster.read( # Note changed order of indexes, arrays are band, row, col order not row, col, band
-            out_shape=(raster.count, height, width),
-            resampling=Resampling.bilinear,
-        )
+    data = raster.read(  # Note changed order of indexes, arrays are band, row, col order not row, col, band
+        out_shape=(raster.count, height, width),
+        resampling=Resampling.bilinear,
+    )
     data[data == -32767] = np.float32(0.2)
-    data = data*scaleHeight
+    data = data * scaleHeight
+
+    newFile = str(name[:-4] + "-scaled.tif")
     with open(
-        r'/mnt/data2/20220719/halle_water_20220719-01_20220719T1546_dsm-rescaled.tif',
-        'w',
+        newFile,
+        "w",
         driver=raster.driver,
         height=height,
         width=width,
         count=raster.count,
-        crs = raster.crs,
-        nodata = raster.nodata,
-        transform = transform,
-        dtype = np.float32,
+        crs=raster.crs,
+        nodata=raster.nodata,
+        transform=transform,
+        dtype=np.float32,
     ) as dest_file:
         dest_file.write(data)
     dest_file.close()
-    
-    return data
 
-def resample_orto(raster, scaleHeight=40, scaleResolution = 1):
+
+def resample_orto(raster, name, scaleHeight=40, scaleResolution=1):
     t = raster.transform
 
     # rescale the metadata
-    transform = Affine(t.a * scaleHeight/scaleResolution, t.b, t.c, t.d, t.e * scaleHeight/scaleResolution, t.f)
+    transform = Affine(
+        t.a * scaleHeight / scaleResolution, t.b, t.c, t.d, t.e * scaleHeight / scaleResolution, t.f
+    )
     height = int(raster.height * scaleResolution)
     width = int(raster.width * scaleResolution)
 
     profile = raster.profile
-    profile.update(transform=transform, driver='GTiff', height=height, width=width)
+    profile.update(transform=transform, driver="GTiff", height=height, width=width)
 
-    data = raster.read( # Note changed order of indexes, arrays are band, row, col order not row, col, band
-            out_shape=(raster.count, height, width),
-            resampling=Resampling.bilinear,
-        )
-        
+    data = raster.read(  # Note changed order of indexes, arrays are band, row, col order not row, col, band
+        out_shape=(raster.count, height, width),
+        resampling=Resampling.bilinear,
+    )
+
+    newFile = str(name[:-4] + "-scaled.tif")
     with open(
-        r'/mnt/data2/20220719/halle_water_20220719-01_20220719T1546_dsm-rescaled-orto.tif',
-        'w',
+        newFile,
+        "w",
         driver=raster.driver,
         height=height,
         width=width,
         count=raster.count,
-        crs = raster.crs,
-        nodata = raster.nodata,
-        transform = transform,
-        dtype = np.float32,
+        crs=raster.crs,
+        nodata=raster.nodata,
+        transform=transform,
+        dtype=np.float32,
     ) as dest_file:
         dest_file.write(data)
     dest_file.close()
-    
- 
 
-    return data
+
+def scaleOrto(ortoPath):
+    ortoFiles = sorted(listdir(ortoPath))
+    for file in ortoFiles:
+
+        if file.endswith(".tif"):
+            ortoName = join(ortoPath, file)
+            ortoFile = open(ortoName)
+            resample_orto(ortoFile, ortoName)
+            print(file)
+
+
+def scaleRaster(rasterPath):
+    rasterFiles = sorted(listdir(rasterPath))
+
+    for file in rasterFiles:
+
+        if file.endswith(".tif"):
+            rasterName = join(rasterPath, file)
+            rasterFile = open(rasterName)
+            resample_raster(rasterFile, rasterName)
+            print(file)
